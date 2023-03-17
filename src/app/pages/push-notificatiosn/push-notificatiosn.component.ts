c9516a8ea4bead23e83ea05718d5b4dca2647465
Ricardo <riky.rm58@gmail.com>
2023-03-17 15:46:09 -0600

    creación del servicio notifications

--- src/app/pages/push-notificatiosn/push-notificatiosn.component.ts
+++ src/app/pages/push-notificatiosn/push-notificatiosn.component.ts
@@ -1,14 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { Router } from '@angular/router';
-
-import {
-  ActionPerformed,
-  PushNotificationSchema,
-  PushNotifications,
-  Token,
-} from '@capacitor/push-notifications';
-
+import { NotificationService } from 'src/app/services/notification.service';
 @Component({
   selector: 'app-push-notificatiosn',
   templateUrl: './push-notificatiosn.component.html'
@@ -16,64 +7,13 @@ import {
 export class PushNotificatiosnComponent implements OnInit{
   tokenString = '';
   permisos = '';
-  constructor(public plataform: Platform,
-    public router: Router){}
-  ngOnInit() {
-    console.log('Initializing HomePage');
-
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    if(this.plataform.is('capacitor')){
-      PushNotifications.requestPermissions().then(result => {
-        if (result.receive === 'granted') {
-          console.log('permisos concedidos');
-          this.permisos = 'true---' + result.receive;
-          // Register with Apple / Google to receive push via APNS/FCM
-          PushNotifications.register();
-          this.addListeners();
-        } else {
-          // Show some error
-          this.permisos = 'false';
-        }
-      });
-    }else{
-      console.log('no es movil')
-    }
-    /*
-    
-    */
+  
+  constructor(private notificationService: NotificationService){}
+  async ngOnInit() {
+    await this.notificationService.requesPermission();
+    this.notificationService.addListeners();
+    this.tokenString = this.notificationService.tokenString;
+    this.permisos = this.notificationService.permisos;
   }
-  addListeners(){
-    // Registro de token del dispositivo
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-        console.log('Push registration success, token: ' + token.value);
-        this.tokenString = token.value;
-      }
-    );
-
-    // Error en el registro
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        console.log('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    // recibir notificación push en primer plano
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotificationSchema) => {
-        console.log('Push received: ' + JSON.stringify(notification));
-
-      }
-    );
-
-    // Metodo caundo se de clic en una notificaíon push
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification: ActionPerformed) => {
-        console.log('Push action performed: ' + JSON.stringify(notification));
-        this.router.navigate(['/push-notificatiosn'])
-      }
-    );
-  }
+  
 }
---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  tokenString = '';
+  permisos = '';
+  constructor(public plataform: Platform,
+    public router: Router) { }
+  
+  requesPermission(){
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    if(this.plataform.is('capacitor')){
+      PushNotifications.requestPermissions().then(result => {
+        if (result.receive === 'granted') {
+          console.log('permisos concedidos');
+          this.permisos = 'true---' + result.receive;
+          // Register with Apple / Google to receive push via APNS/FCM
+          PushNotifications.register();
+        } else {
+          // Show some error
+          this.permisos = 'false';
+        }
+      });
+    }else{
+      console.log('no es movil')
+    }
+  }
+  addListeners(){
+    // Registro de token del dispositivo
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        console.log('Push registration success, token: ' + token.value);
+        this.tokenString = token.value;
+      }
+    );
+
+    // Error en el registro
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        console.log('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // recibir notificación push en primer plano
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        console.log('Push received: ' + JSON.stringify(notification));
+
+      }
+    );
+
+    // Metodo caundo se de clic en una notificaíon push
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        console.log('Push action performed: ' + JSON.stringify(notification));
+        this.router.navigate(['/push-notificatiosn'])
+      }
+    );
+  }
+}
+
